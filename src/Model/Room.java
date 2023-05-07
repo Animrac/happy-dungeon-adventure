7cb8fec9d9f60d3e1e6e867e919e385d52c8d1a8
Hui Wagner <huiwagne@uw.edu>
2023-05-06 23:07:14 -0700

    Added toString, getContentChar, setters and getters method in Room class.

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,89 +1,198 @@
 package src.Model;
+import java.util.Random;
 
 public class Room {
+    private static final int HEALING_POTION_CHANCE = 10;
+    private static final int VISION_POTION_CHANCE = 10;
+    private static final int PIT_CHANCE = 10;
 
-//- visionPotion : boolean
-//- healPotion : boolean
-//- pit : boolean
-//- exit : boolean
-//- monster : boolean
-//- pillar : boolean
-
-    private boolean hasVisionPotion;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
     private boolean hasHealingPotion;
+    private boolean hasVisionPotion;
     private boolean hasPit;
-    private boolean hasEntrance;
-    private boolean hasExit;
-    private boolean hasMonster;
+    private char pillar;
     private boolean hasPillar;
+    private boolean isEntrance;
+    private boolean isExit;
 
-    public Room() {
-    this.hasVisionPotion = false;
-    this.hasHealingPotion = false;
-    this.hasPit = false;
-    this.hasEntrance = false;
-    this.hasExit = false;
-    this.hasMonster = false;
-    this.hasPillar = false;
+    // Room constructor
+    public Room(){
+
+        Random random = new Random();
+
+        hasHealingPotion = Math.random() < (HEALING_POTION_CHANCE / 100.0);
+        hasVisionPotion = Math.random() < (VISION_POTION_CHANCE / 100.0);
+        hasPit = Math.random() < (PIT_CHANCE / 100.0);
+
+        // Initialize the pillar and hasPillar properties
+        pillar = ' ';
+        hasPillar = false;
+
+        // Initialize the entrance and exit properties to false
+        isEntrance = false;
+        isExit = false;
     }
 
-/**
- * Getters and Setters
- * */
+    // Generates a string representation of the room
+    @Override
+    public String toString() {
+        String top;
+        String bottom;
 
-    public boolean hasVisionPotion() {
-        return hasVisionPotion;
+        // Check if there is a north room, if not, display wall (***), otherwise display a door (*-*)
+        if (north == null) {
+            top = "***";
+        } else {
+            top = "*-*";
+        }
+
+        // Check if there is a south room, if not, display wall (***), otherwise display a door (*-*)
+        if (south == null) {
+            bottom = "***";
+        } else {
+            bottom = "*-*";
+        }
+
+        // Check if there is a west room, if not, display wall (*), otherwise display a door (|)
+        char westChar;
+        if (west == null) {
+            westChar = '*';
+        } else {
+            westChar = '|';
+        }
+
+        // Check if there is an east room, if not, display wall (*), otherwise display a door (|)
+        char eastChar;
+        if (east == null) {
+            eastChar = '*';
+        } else {
+            eastChar = '|';
+        }
+
+        // Get the content character (item or empty) for the current room
+        char contentChar = getContentChar();
+
+        // Build the middle row of the room representation
+        String middle = westChar + Character.toString(contentChar) + eastChar;
+
+        // Combine top, middle, and bottom rows to create the full room representation
+        return top + "\n" + middle + "\n" + bottom;
     }
 
-    public void setVisionPotion(boolean hasVisionPotion) {
-        this.hasVisionPotion = hasVisionPotion;
+    // Determine the content character for the room
+    private char getContentChar() {
+        if (isEntrance) {
+            return 'i';
+        } else if (isExit) {
+            return 'O';
+        } else if (hasPillar) {
+            return pillar;
+        }else if (hasHealingPotion && hasVisionPotion && hasPit) {
+                return 'M';
+        } else if (hasHealingPotion) {
+                return 'H';
+        } else if (hasVisionPotion) {
+                return 'V';
+        } else if (hasPit) {
+                return 'X';
+        } else if (pillar != ' ') {
+                return pillar;
+        }
+        return ' ';
     }
 
-    public boolean hasHealingPotion() {
-        return hasHealingPotion;
+        public boolean isEntrance () {
+            return isEntrance;
+        }
+
+        public void setEntrance ( boolean isEntrance){
+            this.isEntrance = isEntrance;
+        }
+
+        public boolean isExit () {
+            return isExit;
+        }
+
+        public void setExit ( boolean isExit){
+            this.isExit = isExit;
+        }
+
+        // Getters and setters
+        public Room getNorth () {
+            return north;
+        }
+
+        public void setNorth (Room north){
+            this.north = north;
+        }
+
+        public Room getSouth () {
+            return south;
+        }
+
+        public void setSouth (Room south){
+            this.south = south;
+        }
+
+        public Room getEast () {
+            return east;
+        }
+
+        public void setEast (Room east){
+            this.east = east;
+        }
+
+        public Room getWest () {
+            return west;
+        }
+
+        public void setWest (Room west){
+            this.west = west;
+        }
+
+
+        // Getter and setter methods for other properties
+        public boolean isHasHealingPotion () {
+            return hasHealingPotion;
+        }
+
+        public void setHasHealingPotion ( boolean hasHealingPotion){
+            this.hasHealingPotion = hasHealingPotion;
+        }
+
+        public boolean isHasVisionPotion () {
+            return hasVisionPotion;
+        }
+
+        public void setHasVisionPotion ( boolean hasVisionPotion){
+            this.hasVisionPotion = hasVisionPotion;
+        }
+
+        public boolean isHasPit () {
+            return hasPit;
+        }
+
+        public void setHasPit ( boolean hasPit){
+            this.hasPit = hasPit;
+        }
+
+        public char getPillar () {
+            return pillar;
+        }
+
+        public void setPillar ( char pillar){
+            this.pillar = pillar;
+        }
+
+        public boolean isHasPillar () {
+            return hasPillar;
+        }
+
+        public void setHasPillar ( boolean hasPillar){
+            this.hasPillar = hasPillar;
+        }
     }
 
-    public void setHealingPotion(boolean hasHealingPotion) {
-        this.hasHealingPotion = hasHealingPotion;
-    }
-
-    public boolean hasPit() {
-        return hasPit;
-    }
-
-    public void setPit(boolean hasPit) {
-        this.hasPit = hasPit;
-    }
-
-    public boolean hasEntrance() {
-        return hasEntrance;
-    }
-
-    public void setEntrance(boolean hasEntrance) {
-        this.hasEntrance = hasEntrance;
-    }
-
-    public boolean hasExit() {
-        return hasExit;
-    }
-
-    public void setExit(boolean hasExit) {
-        this.hasExit = hasExit;
-    }
-
-    public boolean hasMonster() {
-        return hasMonster;
-    }
-
-    public void setMonster(boolean hasMonster) {
-        this.hasMonster = hasMonster;
-    }
-
-    public boolean hasPillar() {
-        return hasPillar;
-    }
-
-    public void setPillar(boolean hasPillar) {
-        this.hasPillar = hasPillar;
-    }
-}
