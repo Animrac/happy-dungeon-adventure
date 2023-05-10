302e767d508876f4e3afb798da663f04adcf246f
Carmina <carminacruz08@yahoo.com>
2023-05-10 00:22:13 -0700

    added JavaFX library and dependency and tried making a little window and button with label

--- src/Model/DungeonAdventure.java
+++ src/Model/DungeonAdventure.java
@@ -1,5 +1,16 @@
 package src.Model;
 
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,12 +20,43 @@ import java.util.Scanner;
  * @author Anastasia Vilenius, Carmina Cruz, Hui Wagner
  * @version 05/9/23
  */
-public class DungeonAdventure {
+public class DungeonAdventure extends Application {
 
     /**
      * Random object used to generate random monsters.
      */
     private static final Random MY_RANDOM = new Random();
+
+
+    Button button;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Happy Dungeon Adventure!");
+        Label intro = new Label(displayIntro());
+
+        button = new Button();
+        button.setText("click me");
+
+        button.setOnAction(e -> System.out.println("you just clicked me!"));
+//        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(intro);
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    //if we did this, we have to implement EventHandler, but lambda expressions seem easier
+//    @Override
+//    public void handle(Event event) {
+//        if (event.getSource() == button){
+//            //do something
+//        }
+//    }
 
     /**
      * Driver method for class (and entire game)
@@ -23,61 +65,83 @@ public class DungeonAdventure {
      *
      * @param theArgs
      */
-    public static void main(String[] args) { //this main method is just to test
+    public static void main(String[] theArgs) { //this main method is just to test
         displayIntro();
-        Dungeon layout = null;
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("--Would you like a custom dungeon? Y/N--");
-        String custom = input.nextLine();  // Read user input
-
-        if (custom.equals("Y") || custom.equals("y") || custom.equals("yes") || custom.equals("Yes")) { //i know there are better ways but im lazy rn
-            System.out.println("--Rows (must be at least 5):--");
-            int cRows = Integer.parseInt(input.nextLine()) + 2;
-            System.out.println("--Columns (must be at least 5):--");
-            int cCols = Integer.parseInt(input.nextLine()) + 2;
-            layout = new Dungeon(cRows, cCols);
-        } else {
-            layout = new Dungeon();
-        }
-
-        //TODO: USE REGEX FOR INVALID INPUT
-        //VISUALIZATION TEST CODE
-        System.out.println("Dungeon:");
-        for (int i = 0; i < layout.getMyDungeonLayout().length; i++) {
-            for (int j = 0; j < layout.getMyDungeonLayout()[i].length; j++) {
-                System.out.print(layout.getMyDungeonLayout()[i][j]);
-            }
-            System.out.println();
-        }
-
-
-        //getMyDungeonRooms()[y][x]
-        System.out.println();
-
-        System.out.println("Room (1,1):");
-        System.out.print(layout.getMyDungeonRooms()[1][1].toString());
-
-        System.out.println();
-
-        System.out.println("Room (3,5):");
-        System.out.print(layout.getMyDungeonRooms()[5][3].toString());
-
-        System.out.println();
-
-        System.out.println("Room (2,4):");
-        System.out.print(layout.getMyDungeonRooms()[4][2].toString());
-
-        //PRINT TEST ALL THE ROOMS EVERYWHERE, this does not include the boundary
-        for (int i = 1; i < layout.getMyDungeonRooms().length; i++) {
-            for (int j = 1; j < layout.getMyDungeonRooms()[i].length; j++) {
-                System.out.println("Room (" + j + ", " + i + ")");
-                System.out.println(layout.getMyDungeonRooms()[i][j]);
-            }
-            System.out.println();
-        }
-
-        gamePlay();
+        launch(theArgs);
+//        //1. Create the frame.
+//        JFrame frame = new JFrame("Happy Dungeon Adventure!");
+//        frame.setBounds(600, 400, 600, 400);
+//
+//        //Set the frame icon to an image loaded from a file.
+////        frame.setIconImage(new ImageIcon(imgURL).getImage());
+//
+//        //2. Optional: What happens when the frame closes?
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        //3. Create components and put them in the frame.
+//        //...create emptyLabel...
+////        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+//
+//        //4. Size the frame.
+//        frame.pack();
+//
+//        //5. Show it.
+//        frame.setVisible(true);
+//
+//        Dungeon layout = null;
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("--Would you like a custom dungeon? Y/N--");
+//        String custom = input.nextLine();  // Read user input
+//
+//        if (custom.equals("Y") || custom.equals("y") || custom.equals("yes") || custom.equals("Yes")) { //i know there are better ways but im lazy rn
+//            System.out.println("--Rows (must be at least 5):--");
+//            int cRows = Integer.parseInt(input.nextLine()) + 2;
+//            System.out.println("--Columns (must be at least 5):--");
+//            int cCols = Integer.parseInt(input.nextLine()) + 2;
+//            layout = new Dungeon(cRows, cCols);
+//        } else {
+//            layout = new Dungeon();
+//        }
+//
+//        //TODO: USE REGEX FOR INVALID INPUT
+//        //VISUALIZATION TEST CODE
+//        System.out.println("Dungeon:");
+//        for (int i = 0; i < layout.getMyDungeonLayout().length; i++) {
+//            for (int j = 0; j < layout.getMyDungeonLayout()[i].length; j++) {
+//                System.out.print(layout.getMyDungeonLayout()[i][j]);
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        //getMyDungeonRooms()[y][x]
+//        System.out.println();
+//
+//        System.out.println("Room (1,1):");
+//        System.out.print(layout.getMyDungeonRooms()[1][1].toString());
+//
+//        System.out.println();
+//
+//        System.out.println("Room (3,5):");
+//        System.out.print(layout.getMyDungeonRooms()[5][3].toString());
+//
+//        System.out.println();
+//
+//        System.out.println("Room (2,4):");
+//        System.out.print(layout.getMyDungeonRooms()[4][2].toString());
+//
+//        //PRINT TEST ALL THE ROOMS EVERYWHERE, this does not include the boundary
+//        for (int i = 1; i < layout.getMyDungeonRooms().length; i++) {
+//            for (int j = 1; j < layout.getMyDungeonRooms()[i].length; j++) {
+//                System.out.println("Room (" + j + ", " + i + ")");
+//                System.out.println(layout.getMyDungeonRooms()[i][j]);
+//            }
+//            System.out.println();
+//        }
+//
+//        gamePlay();
 
     }
 
@@ -94,12 +158,14 @@ public class DungeonAdventure {
     /**
      * Prints out an introduction to the game.
      */
-    public static void displayIntro() {
-        System.out.println("You are about to begin a Happy Adventure!");
-        System.out.println("\nBut wait!!!"
+    public static String displayIntro() {
+        StringBuilder s = new StringBuilder();
+        s.append("You are about to begin a Happy Adventure!");
+        s.append("\nBut wait!!!"
                 + "\n\nUnfortunately, you were thrown in a dungeon by the scary Tom Nook because you forgot to pay your bills on time. "
                 + "\n\nNow, you must venture on a dangerous expedition to collect the four pillars of OO, all while paying your rent to make it out of the dungeon alive."
                 + "\n\nGood luck! You'll need it!\n");
+        return s.toString();
     }
 
     /**
@@ -194,4 +260,7 @@ public class DungeonAdventure {
 
         battle(hero, monster);
     }
+
+
+
 }
