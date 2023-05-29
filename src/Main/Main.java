3480e3a2deaf572bfa599acb88daec7c12c05cf4
Carmina <carminacruz08@yahoo.com>
2023-05-29 11:00:18 -0700

    cleaned up Main and removed unecessary files

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,11 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -20,21 +16,6 @@ import java.io.IOException;
 
 public class Main extends Application {
     private static Stage primaryStage;
-
-    @FXML
-    private MenuBar myMenuBar;
-
-    @FXML
-    private MenuItem menuControls;
-
-    @FXML
-    private AnchorPane currPane;
-
-    @FXML
-    private AnchorPane rootPane;
-
-    @FXML
-    private VBox mainVBox;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -49,14 +30,6 @@ public class Main extends Application {
             Scene scene = SceneMaker.createScene("src/View/start.fxml");
 
             primaryStage.setScene(scene);
-//            primaryStage.setMaxHeight(400);
-//            primaryStage.setMinHeight(400);
-//            primaryStage.setMaxWidth(600);
-//            primaryStage.setMinWidth(600);
-//            primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
-//                if (newValue)
-//                    primaryStage.setMaximized(false);
-//            });
             primaryStage.setResizable(false);
             primaryStage.show();
         }
@@ -81,10 +54,6 @@ public class Main extends Application {
      */
     public static void main(String[] theArgs) {
         launch(theArgs);
-
-        //Console Version of the Game
-//        gamePlay();
-
     }
 
     @FXML
