acea353ca95fd69a7f2b93bc5ae8bcc2906d8eaa
Carmina <carminacruz08@yahoo.com>
2023-05-30 11:07:29 -0700

    MonsterFactory.java added and commented out

--- src/Model/MonsterFactory.java
+++ src/Model/MonsterFactory.java
@@ -1,116 +1,116 @@
-package src.Model;
-
-import org.sqlite.SQLiteDataSource;
-
-import java.sql.*;
-import java.util.*;
-
-/**
- * This program stores the types of monsters in a database
- *
- * @author Anastasia Vilenius
- * @version 05/28/23
- */
-public class MonsterFactory {
-
-    SQLiteDataSource ds = null;
-
-    //private static final MonsterFactory mf = new MonsterFactory();
-    private static java.sql.Connection connection;
-
-    private MonsterFactory() {
-        //establish connection (creates db file if it does not exist :-)
-        try {
-            ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:monsterFactory.db");
-        } catch ( Exception e ) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        System.out.println( "Opened database successfully" );
-
-
-        //now create a table
-        String query = "CREATE TABLE IF NOT EXISTS monsterFactory ( " +
-            "NAME string," +
-            "HEALTH int," +
-            "MIN_DAMAGE int," +
-            "MAX_DAMAGE int," +
-            "ATTACK_ODDS double," +
-            "BLOCK_ODDS double," +
-            "ATTACK_SPEED int)";
-
-        try {
-            Connection connection = ds.getConnection();
-            Statement stmt = connection.createStatement();
-            int rv = stmt.executeUpdate( query );
-            System.out.println( "executeUpdate() returned " + rv );
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-            System.exit( 0 );
-        }
-        System.out.println( "Created monsterFactory table successfully" );
-
-        //next insert 3 rows of data
-        System.out.println( "Attempting to insert 3 rows into monsterFactory table" );
-
-        String query1 = "INSERT INTO monsterFactory ('Monster One', 80, 20, 40, .8, 3, 3)";
-        String query2 = "INSERT INTO monsterFactory ('Monster Two', 80, 20, 40, .8, 3, 3)";
-        String query3 = "INSERT INTO monsterFactory ('Monster Three', 80, 20, 40, .8, 3, 3)";
-
-
-        try (Connection conn = ds.getConnection();
-            Statement stmt = conn.createStatement(); ) {
-            int rv = stmt.executeUpdate( query1 );
-            System.out.println( "1st executeUpdate() returned " + rv );
-
-            rv = stmt.executeUpdate( query2 );
-            System.out.println( "2nd executeUpdate() returned " + rv );
-
-            rv = stmt.executeUpdate( query3 );
-            System.out.println( "3rd executeUpdate() returned " + rv );
-
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-            System.exit( 0 );
-        }
-
-
-        //now query the database table for all its contents and display the results
-        int row = 0;
-        String theName = "";
-
-        switch (theName) {
-            case ("Monster One") -> row = 1;
-            case ("Monster Two") -> row = 2;
-            case ("Monster Three") -> row = 3;
-
-            default -> {
-                throw new NoSuchElementException(theName + "is not a valid monster");
-            }
-
-        }
-        query = "SELECT * FROM monsterFactory WHERE row = " + row;
-
-        try ( Connection conn = ds.getConnection();
-              Statement stmt = conn.createStatement()) {
-
-            ResultSet rs = stmt.executeQuery(query);
-
-            new Monster(
-                    rs.getString("NAME"),
-                    rs.getInt("HEALTH"),
-                    rs.getInt("MIN_DAMAGE"),
-                    rs.getInt("MAX_DAMAGE"),
-                    rs.getDouble("ATTACK_ODDS"),
-                    rs.getDouble("BLOCK_ODDS"),
-                    rs.getInt("ATTACK_SPEED")
-            );
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-            System.exit( 0 );
-        }
-    }
-
-}
+//package src.Model;
+//
+//import org.sqlite.SQLiteDataSource;
+//
+//import java.sql.*;
+//import java.util.*;
+//
+///**
+// * This program stores the types of monsters in a database
+// *
+// * @author Anastasia Vilenius
+// * @version 05/28/23
+// */
+//public class MonsterFactory {
+//
+//    SQLiteDataSource ds = null;
+//
+//    //private static final MonsterFactory mf = new MonsterFactory();
+//    private static java.sql.Connection connection;
+//
+//    private MonsterFactory() {
+//        //establish connection (creates db file if it does not exist :-)
+//        try {
+//            ds = new SQLiteDataSource();
+//            ds.setUrl("jdbc:sqlite:monsterFactory.db");
+//        } catch ( Exception e ) {
+//            e.printStackTrace();
+//            System.exit(0);
+//        }
+//
+//        System.out.println( "Opened database successfully" );
+//
+//
+//        //now create a table
+//        String query = "CREATE TABLE IF NOT EXISTS monsterFactory ( " +
+//            "NAME string," +
+//            "HEALTH int," +
+//            "MIN_DAMAGE int," +
+//            "MAX_DAMAGE int," +
+//            "ATTACK_ODDS double," +
+//            "BLOCK_ODDS double," +
+//            "ATTACK_SPEED int)";
+//
+//        try {
+//            Connection connection = ds.getConnection();
+//            Statement stmt = connection.createStatement();
+//            int rv = stmt.executeUpdate( query );
+//            System.out.println( "executeUpdate() returned " + rv );
+//        } catch ( SQLException e ) {
+//            e.printStackTrace();
+//            System.exit( 0 );
+//        }
+//        System.out.println( "Created monsterFactory table successfully" );
+//
+//        //next insert 3 rows of data
+//        System.out.println( "Attempting to insert 3 rows into monsterFactory table" );
+//
+//        String query1 = "INSERT INTO monsterFactory ('Monster One', 80, 20, 40, .8, 3, 3)";
+//        String query2 = "INSERT INTO monsterFactory ('Monster Two', 80, 20, 40, .8, 3, 3)";
+//        String query3 = "INSERT INTO monsterFactory ('Monster Three', 80, 20, 40, .8, 3, 3)";
+//
+//
+//        try (Connection conn = ds.getConnection();
+//            Statement stmt = conn.createStatement(); ) {
+//            int rv = stmt.executeUpdate( query1 );
+//            System.out.println( "1st executeUpdate() returned " + rv );
+//
+//            rv = stmt.executeUpdate( query2 );
+//            System.out.println( "2nd executeUpdate() returned " + rv );
+//
+//            rv = stmt.executeUpdate( query3 );
+//            System.out.println( "3rd executeUpdate() returned " + rv );
+//
+//        } catch ( SQLException e ) {
+//            e.printStackTrace();
+//            System.exit( 0 );
+//        }
+//
+//
+//        //now query the database table for all its contents and display the results
+//        int row = 0;
+//        String theName = "";
+//
+//        switch (theName) {
+//            case ("Monster One") -> row = 1;
+//            case ("Monster Two") -> row = 2;
+//            case ("Monster Three") -> row = 3;
+//
+//            default -> {
+//                throw new NoSuchElementException(theName + "is not a valid monster");
+//            }
+//
+//        }
+//        query = "SELECT * FROM monsterFactory WHERE row = " + row;
+//
+//        try ( Connection conn = ds.getConnection();
+//              Statement stmt = conn.createStatement()) {
+//
+//            ResultSet rs = stmt.executeQuery(query);
+//
+//            new Monster(
+//                    rs.getString("NAME"),
+//                    rs.getInt("HEALTH"),
+//                    rs.getInt("MIN_DAMAGE"),
+//                    rs.getInt("MAX_DAMAGE"),
+//                    rs.getDouble("ATTACK_ODDS"),
+//                    rs.getDouble("BLOCK_ODDS"),
+//                    rs.getInt("ATTACK_SPEED")
+//            );
+//        } catch ( SQLException e ) {
+//            e.printStackTrace();
+//            System.exit( 0 );
+//        }
+//    }
+//
+//}
